9613507190104c56deed66291af0946a3c99255a
akhilbju <abiju60@gmail.com>
2025-11-13 23:18:24 +0530

    some improvement

--- DAOModels/CreateUser.cs
+++ DAOModels/CreateUser.cs
@@ -2,7 +2,7 @@ namespace Auth_service.DAOModels
 {
     public class CreateUser
     {
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string RePassword { get; set; }
